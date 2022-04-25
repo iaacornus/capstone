c400ecf5606d829a312fd14f9e9cf74f83c2a342
testno0 <jaaaderang@gmail.com>
2022-04-25 21:00:03 +0800

    fixed the nulls and errors

--- src/algorithm/function.py
+++ src/algorithm/function.py
@@ -1,35 +1,36 @@
 from os import system as sys
+from os.path import exists
 
 import json
 
 class System:
-    def __init__(self, PWD, repo):
-        self.PWD = PWD
+    def __init__(self, HOME, repo):
+        self.HOME = HOME
         self.repo = repo
         
-    def pullData(self, PWD, repo):
+    def pullData(self):
         try:
-            if exits(f"{PWD}/<filename>") is True:
-                sys("rm <filename>")
-            sys(f"git clone {repo}")
+            if exists(f"{self.HOME}/capstone") is True:
+                sys(f"rm -rf {self.HOME}/capstone")
+            sys(f"git clone --branch database {self.repo}")
         
             return True
-        except SystemError, KeyboardInterrupt, OSError, ConnectionError:
+        except SystemError or KeyboardInterrupt or OSError or ConnectionError:
             return False
             
-    def getData():
+    def getData(self):
         try:
-            with open("<filename>") as data:
+            with open(f"{self.HOME}/capstone/<filename>") as data:
                 studentDATA = json.load(data)
                 
-            with open("<filename>") as Data:
+            with open(f"{self.HOME}/capstone/<filename>") as Data:
                 teacherDATA = json.load(Data)
                 
             return studentDATA, teacherDATA
             
         except FileNotFoundError:
             while True:
-                if pullData() is True:
+                if self.pullData() is True:
                     return True, True
                 else:
                     continue
