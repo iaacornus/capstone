33797d25deb59cd3691c7c7dcdad74305d9eb813
testno0 <jaaaderang@gmail.com>
2022-04-28 22:55:59 +0800

    fixed errors

--- src/system/setup.sh
+++ src/system/setup.sh
@@ -1,7 +1,9 @@
-if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
+# test 1: #* passed!
+
+if [ ! -d "$HOME/.att_sys/" ]; then
     # system upgrade: #* pass
     echo -e "\e[1;32m> Starting full system upgrade to fix CVE vulnerabilities ...\e[0m\nKindly input the current password : 'root' (no quotations) when prompted, and don't let the system die."
-    sudo dnf update -y
+    #sudo dnf update -y
     
     # check for system utilities: #? likely passing    
     echo -e "\e[1;32m> Checking the presence of git, installing if not installed ...\e[0m"    
@@ -16,8 +18,8 @@ if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
     echo -e "\e[1;32m> Checking the presence of python.pip, installing if not installed ...\e[0m"
     pip_check=$(dnf list all | grep "python3-pip")
  
-    if [[ $pip_check != *"python.pip"* ]]; then
-        echo -e "\e[1;32m> Installing git in the system ...\e[0m"
+    if [[ $pip_check != *"python3-pip"* ]]; then
+        echo -e "\e[1;32m> Installing python3-pip in the system ...\e[0m"
         sudo dnf install python3-pip
 
     fi
@@ -28,11 +30,11 @@ if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
 
     # setup a systemd service for repository check : #! on test
     echo -e "\e[1;32m> Setting up a systemd service ...\e[0m"
-    sudo touch /etc/systemd/system/repository-update.service
+    sudo touch /etc/systemd/system/repository-check.service
     echo -e "[Unit]\nDescription=Check the repository for updates every 24 hours.\nAfter=network.target\nStartLimitIntervalSec=5\n\n[Service]\nType=simple\nRestart=always\nRestartSec=5\nUser='exec $USER'\nExecStart=/usr/bin/env python 'exec $HOME/repository/bin/service.py'\n\n[Install]\nWantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/repository-check.service
 
     sudo systemctl daemon-reload
-    sudo systemctl enable system-integrity.service
+    sudo systemctl enable repository-check.service
 
     # setup the dirs
     mkdir $HOME/.att_sys $HOME/.att_sys/bin $HOME/.att_sys/system $HOME/.att_sys/system/utils $HOME/.att_sys/misc $HOME/.att_sys/algorithm
@@ -48,7 +50,7 @@ if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
     # utils@$HOME/.att_sys/system/utils
     mv $HOME/capstone/src/system/utils/* $HOME/.att_sys/system/utils
     # download the setup script instead of moving it
-    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/setup.sh -P $HOME/.att_sys/system/setup.sh
+    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/setup.sh -P $HOME/.att_sys/system/
 
     # misc@HOME/.att_sys/misc
     mv $HOME/capstone/src/misc/* $HOME/.att_sys/misc
@@ -56,7 +58,7 @@ if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
 
     # remove the old bashrc
     rm $HOME/.bashrc
-    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/.bashrc -P $HOME/.bashrc
+    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/.bashrc -P $HOME/
     source $HOME/.bashrc
 
     # user information setup : #* pass
@@ -69,14 +71,14 @@ if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
     read -p "└─╼ Enter the school name: " school_name
     
     # generate user password
-    password=$(python $HOME/capstone/src/system/utils/password_gen.py)
+    password=$(python $HOME/.att_sys/system/utils/password_gen.py)
     echo -e "\e[1;32m> Your password is: $password"
 
     # append user information and password to a file
     echo -e "\e[1;32m> Appending user info ...\e[0m"
     echo -e "$email\n$user_name\n$password\n$school_name" >> $HOME/.att_sys/user_info
 
-    access=$(bash $HOME/capstone/src/system/utils/update.sh)
+    access=$(bash $HOME/.att_sys/system/utils/update.sh)
 
     sec=10
     while [ $sec -ge 0 ]; do
