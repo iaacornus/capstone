1c4fca9076834ba29b3b4414f410e0c13c478c0e
testno0 <jaaaderang@gmail.com>
2022-04-28 23:35:07 +0800

    print was used instead of return to store the variable in bash script

--- src/system/utils/password_gen.py
+++ src/system/utils/password_gen.py
@@ -1,8 +1,4 @@
 import random
 import string
 
-def main():
-    return ''.join([random.choice(random.choice([string.ascii_lowercase, string.ascii_uppercase, string.punctuation, string.digits])) for x in range(32)])
-
-if __name__ == "__main__":
-    main()
+print(''.join([random.choice(random.choice([string.ascii_lowercase, string.ascii_uppercase, string.punctuation, string.digits])) for x in range(32)]))
