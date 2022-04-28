b03b9d6d6b4dc4b2a069419948bb600634d0d74f
testno0 <jaaaderang@gmail.com>
2022-04-28 20:49:14 +0800

    fixed the initialized setup script
    fixed the dir of the src codes, improved the user setup, and fixed the secure setup

--- src/system/setup.sh
+++ src/system/setup.sh
@@ -1,70 +1,92 @@
 if [ ! -d "$HOME/repository/" ] || [ ! -d "$HOME/.att_sys/" ]; then
-    # system upgrade : #* pass
+    # system upgrade: #* pass
     echo -e "\e[1;32m> Starting full system upgrade to fix CVE vulnerabilities ...\e[0m\nKindly input the current password : 'root' (no quotations) when prompted, and don't let the system die."
     sudo dnf update -y
     
-    # check for system utilities : #? likely passing    
-    echo -e "\e[1;32m> Checking system utils, installing if not installed ...\e[0m"
-    
+    # check for system utilities: #? likely passing    
+    echo -e "\e[1;32m> Checking the presence of git, installing if not installed ...\e[0m"    
     git_check=$(dnf list all | grep "git")
-    pip_check=$(dnf list all | grep "python.pip")
-
     if [[ $git_check != *"git"* ]]; then
         echo -e "\e[1;32m> Installing git in the system ...\e[0m"
         sudo dnf install git
 
     fi
 
+    # install pip if not installed: #? likely passing
+    echo -e "\e[1;32m> Checking the presence of python.pip, installing if not installed ...\e[0m"
+    pip_check=$(dnf list all | grep "python3-pip")
+ 
     if [[ $pip_check != *"python.pip"* ]]; then
         echo -e "\e[1;32m> Installing git in the system ...\e[0m"
         sudo dnf install python3-pip
 
     fi
 
-    # install required packages : #? likely passing
+    # install required packages: #? likely passing
     echo -e "\e[1;31m> Installing required packages ...\e[0m"
     pip install -r $HOME/capstone/requirements.txt
 
     # setup a systemd service for repository check : #! on test
-    echo -e "\e[1;32m> Setting a systemd service ...\e[0m"
+    echo -e "\e[1;32m> Setting up a systemd service ...\e[0m"
     sudo touch /etc/systemd/system/repository-update.service
     echo -e "[Unit]\nDescription=Check the repository for updates every 24 hours.\nAfter=network.target\nStartLimitIntervalSec=5\n\n[Service]\nType=simple\nRestart=always\nRestartSec=5\nUser='exec $USER'\nExecStart=/usr/bin/env python 'exec $HOME/repository/bin/service.py'\n\n[Install]\nWantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/repository-check.service
 
     sudo systemctl daemon-reload
     sudo systemctl enable system-integrity.service
 
-    echo -e "\e[1;32m> Starting user setup ...\e[0m"
-    ret=$(python $HOME/repository/bin/access.py)
+    # setup the dirs
+    mkdir $HOME/.att_sys $HOME/.att_sys/bin $HOME/.att_sys/system $HOME/.att_sys/system/utils $HOME/.att_sys/misc $HOME/.att_sys/algorithm
+    touch $HOME/.att_sys/user_info
 
-    if [[ $ret = "False" ]]; then
-        # false phase, pass : #? passing
-        rm -rf $HOME/capstone/*
-        sec=3
-        while [ $sec -ge 0 ]; do
-            echo -e "\e[1;31m> Verification error, shutting the system down" $sec"s ...\e[0m\r"            
-            "\e[32m> Setup done! Rebooting after :" 
-            let "sec=sec-1"
-            sleep 1
-        done
+    # move the binaries: #? likely passing
+    # main binaries@$HOME/.att_sys/bin/
+    mv $HOME/capstone/src/bin/* $HOME/.att_sys/bin/
 
-        systemctl poweroff
-    elif [[ $ret = "True" ]]; then
-        # end phase : #* pass
-        echo -e "\e[32m> User system setup passed.\e[0m"
-        git clone https://github.com/testno0/repo
-        sec=10
-        while [ $sec -ge 0 ]; do
-            echo -e "\e[32m> Setup done! Rebooting after :" $sec"s ...\e[0m\r" 
-            let "sec=sec-1"
-            sleep 1
-        done
+    # algorithm@$HOME/.att_sys/algorithm/
+    mv $HOME/capstone/src/algorithm/* $HOME/.att_sys/algorithm
 
-        systemctl poweroff
+    # utils@$HOME/.att_sys/system/utils
+    mv $HOME/capstone/src/system/utils/* $HOME/.att_sys/system/utils
+    # download the setup script instead of moving it
+    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/setup.sh -P $HOME/.att_sys/system/setup.sh
+
+    # misc@HOME/.att_sys/misc
+    mv $HOME/capstone/src/misc/* $HOME/.att_sys/misc
+    mv $HOME/capstone/requirements.txt $HOME/.att_sys/
+
+    # remove the old bashrc
+    rm $HOME/.bashrc
+    wget https://raw.githubusercontent.com/testno0/capstone/devel/src/system/.bashrc -P $HOME/.bashrc
+    source $HOME/.bashrc
+
+    # user information setup : #* pass
+    echo -e "\e[1;32m> User setup ...\e[0m"
+
+    # take the username and email of the user
+    read -p "└─╼ Enter the email for access codes and reports: " email
+    read -p "└─╼ Enter the username: " user_name
     
-    fi
+    # generate user password
+    password=$(python $HOME/capstone/src/system/utils/password_gen.py)
+    echo -e "\e[1;32m> Your password is: $password"
+
+    # append user information and password to a file
+    echo -e "\e[1;32m> Appending user info ...\e[0m"
+    echo -e "$email\n$user_name\n$password" >> $HOME/.att_sys/user_info
+
+    access=$(bash $HOME/capstone/src/system/utils/update.sh)
+
+    sec=10
+    while [ $sec -ge 0 ]; do
+        echo -e "\e[32m> Setup done! Rebooting after :" $sec"s ...\e[0m\r" 
+        let "sec=sec-1"
+        sleep 1
+    done
+
+    systemctl poweroff
 
 else
     #* passed
-    echo -e "\e[1;31m> System already setup, pulling updates from repository ...\e[0m"
+    echo -e "\e[1;31m> System already setup ...\e[0m"
 
 fi
