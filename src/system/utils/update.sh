b2f2be4bd0f12e29bf6b5b97efb0015085ddcfa7
testno0 <jaaaderang@gmail.com>
2022-04-28 21:31:16 +0800

    enforced the password access, not foolproof, can be easily wrecked by non idiots user, but since the user will be idiots, it should work

--- src/system/utils/update.sh
+++ src/system/utils/update.sh
@@ -1,25 +1,23 @@
 ret=$(python $HOME/repository/bin/access.py)
 
-if [ ! -d "$HOME/repo" ]; then
-    ret=$(python $HOME/.att_sys/bin/access.py)
+ret=$(python $HOME/.att_sys/bin/access.py)
 
-    if [[ $ret = "False" ]]; then
-        # false phase, pass : #? passing
-        rm -rf $HOME/repo/
-        sec=3
-        echo -e "\e[1;31m> Verification error, repository was nuked by system for security."            
-        systemctl poweroff
-    elif [[ $ret = "True" ]]; then
-        # end phase : #* pass
+if [[ $ret = "False" ]]; then
+    if [ ! -d "$HOME/repo" ]; then        
+        git clone https://github.com/testno0/repo $HOME/ &> /dev/null
+
+    else
         echo -e "\e[32m> User system setup passed.\e[0m"
-        git clone https://github.com/testno0/repo
-        sec=10
-        while [ $sec -ge 0 ]; do
-            echo -e "\e[32m> Setup done! Rebooting after :" $sec"s ...\e[0m\r" 
-            let "sec=sec-1"
-            sleep 1
-        done
+        cd $HOME/repo/ && git pull
+        
+    fi
 
-        systemctl poweroff
+else
+  # false phase, pass : #? passing
+    rm -rf $HOME/repo/
+    sec=3
+    echo -e "\e[1;31m> Verification error, repository was nuked by system for security."            
+    systemctl poweroff
 
 fi
+
