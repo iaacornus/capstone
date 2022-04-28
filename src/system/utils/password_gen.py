53a3ede30965ccc42b39f2f13e2e283f4e50b13d
testno0 <jaaaderang@gmail.com>
2022-04-28 23:20:59 +0800

    fixed the formatting

--- src/system/utils/password_gen.py
+++ src/system/utils/password_gen.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def main():
-   return ''.join([random.choice(random.choice([string.ascii_lowercase, string.ascii_uppercase, string.punctuation, string.digits])) for x in range(32)])
+    return ''.join([random.choice(random.choice([string.ascii_lowercase, string.ascii_uppercase, string.punctuation, string.digits])) for x in range(32)])
 
 if __name__ == "__main__":
     main()
