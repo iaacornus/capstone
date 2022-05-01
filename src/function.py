e68e9528852c84df987d8f8cc29e5d6c7471e5de
testno0 <jaaaderang@gmail.com>
2022-05-01 19:44:00 +0800

    moved to src/ instead of src/algorithm
    this was done for easier navigation of the application

---
+++ src/function.py
@@ -0,0 +1,66 @@
+import sys
+sys.path.append(".")
+
+import json
+
+from os import system as sys
+from os.path import exists
+
+from bin.code_email import Email
+
+class System:
+    def __init__(self, HOME, repo, phrase, admin_email):
+        self.HOME = HOME
+        self.repo = repo
+        self.phrase = phrase
+        self.admin_email = admin_email
+        
+    def pullData(self):
+        try:
+            if exists(f"{self.HOME}/capstone") is True:
+                sys(f"rm -rf {self.HOME}/capstone")
+            sys(f"git clone --branch database {self.repo}")
+        
+            return True
+        except SystemError or KeyboardInterrupt or OSError or ConnectionError:
+            return False
+            
+    def getData(self):
+        count = 0
+
+        while True:
+            try:
+                with open(f"{self.HOME}/repo/<filename>") as data:
+                    studentDATA = json.load(data)
+                    
+                with open(f"{self.HOME}/repo/<filename>") as Data:
+                    teacherDATA = json.load(Data)
+                    
+                return studentDATA, teacherDATA
+                
+            except FileNotFoundError:
+                if self.pullData() is True:
+                    continue
+                else:
+                    if count == 3:
+                        raise SystemExit("Too much error, please try again later.")
+                    count += 1
+                    continue
+
+    def setup(self):
+        email = Email(self.admin_email)
+        phrase = email.notify(setup=True)
+
+        if self.phrase != phrase:
+            raise SystemExit("Error")
+        else:
+            try:
+                while True:
+                    if self.pullData() is not True:                    
+                        continue
+                    else:
+                        break
+            except:
+                raise SystemExit("Error")
+            else:
+                studentDATA, teacherDATA = self.getData()
