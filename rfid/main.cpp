1473b0c7320b6758a2f359c690015d3cf3425093
testno0 <96910593+testno0@users.noreply.github.com>
2022-05-10 10:35:55 +0800

    Create main.cpp

---
+++ rfid/main.cpp
@@ -0,0 +1,74 @@
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+String UID = "43 12 9F 94";
+byte lock = 0;
+
+Servo servo;
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+void setup() {
+  Serial.begin(9600);
+  servo.write(70);
+  lcd.init();
+  lcd.backlight();
+  servo.attach(3);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void loop() {
+  lcd.setCursor(6, 1);
+  lcd.print("Welcome!");
+  lcd.setCursor(0, 2);
+  lcd.print("Please scan the card");
+
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Scanning");
+  lcd.setCursor(0, 1);
+  lcd.print("Please wait");
+  Serial.print("NUID tag is :");
+  String ID = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    lcd.print(".");
+    ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    ID.concat(String(rfid.uid.uidByte[i], HEX));
+    delay(300);
+  }
+  ID.toUpperCase();
+
+  if (ID.substring(1) == UID && lock == 0 ) {
+    servo.write(70);
+    lcd.clear();
+    lcd.setCursor(4, 1);
+    lcd.print("Door Locked");
+    delay(1500);
+    lcd.clear();
+    lock = 1;
+  } else if (ID.substring(1) == UID && lock == 1 ) {
+    servo.write(160);
+    lcd.clear();
+    lcd.setCursor(4, 1);
+    lcd.print("Door Opened");
+    delay(1500);
+    lcd.clear();
+    lock = 0;
+  } else {
+    lcd.clear();
+    lcd.setCursor(4, 1);
+    lcd.print("Access Denied");
+    delay(1500);
+    lcd.clear();
+  }
+}
