fa50446f592f10175c1251c894f9ea27c383fcec
iaacornus <iaacornus.devel@gmail.com>
2022-05-10 15:16:42 +0800

    successful face recognition build

--- src/face_recog.py
+++ src/face_recog.py
@@ -1,15 +1,10 @@
-import face_recognition as fr
-import cv2 as cv 
-import numpy as np
 import sys
-from PIL import ImageDraw as id, Image as img
+import face_recognition as fr
+import numpy as np
+import cv2 as cv
 
-from misc.colors import colors
+from os.path import expanduser
 
-C = colors()
-
-# this checks all the available web cams in computer and exits
-# if there is none, and returns True or 1 if there is any
 def av_cams():
     index, arr = 0, []
 
@@ -27,13 +22,100 @@ def av_cams():
         pass
         # raise SystemExit(f"{C.RED+C.BOLD}> No camera available.{C.END}")
     else:
-        input(f"\33[1;32m> All available cameras: {[num+' '+cam for num, cam in enumerate(arr)]}\33[0m\nPress any key to clear ...")
+        input(f"\33[1;32m> All available cameras: {[f'{num} {cam}' for num, cam in enumerate(arr)]}\33[0m\nPress any key to clear ...")
         sys.stdout.write("\033[K")
-        return True
-
-av_cams()
 
 
+def face_recognition():
+    #av_cams()
+    __PATH__ = f"{expanduser('~')}/temporary/capstone/sample/"
+    
+    # load face references from __PATH__.
+    ref_face = fr.load_image_file(f"{__PATH__}/test_1.png")    
+    rf_encoding = fr.face_encodings(ref_face)[0]
 
-# feed = cv.VideoCapture(0)
+    ref_face_2 = fr.load_image_file(f"{__PATH__}/test_2.png")
+    rf_encoding2 = fr.face_encodings(ref_face_2)[0]
 
+    known_fe = [
+        rf_encoding,
+        rf_encoding2,    
+        ]
+    known_fnames = [
+        "Ezekiel Lopez",
+        "Laisie Angela Donato",
+        ]
+        
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+    
+    # video capture
+    vid = cv.VideoCapture(0)
+
+    while True:   
+        # take frame and references from video capture
+        ret, frame = vid.read()
+
+        # resizing to smaller frame, to avoid much larger use of gpu and 
+        # memory, also to decrease the processing time convert to another color.
+        small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # convert to another color.
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        if process_this_frame:
+            # get all the face endcoding and location in the current 
+            # frame returned by the live camera input.
+            face_locations = fr.face_locations(rgb_small_frame)
+            face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = fr.compare_faces(known_fe, face_encoding)
+                name = "unknown"
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = fr.face_distance(known_fe, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_fnames[best_match_index]
+
+                face_names.append(name)
+
+            for (top, right, bottom, left) in face_locations:
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # output the box in the frame
+                if name != "unknown":
+                    cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+                    # Draw a label with a name below the face
+                    cv.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv.FILLED)
+                    font = cv.FONT_HERSHEY_DUPLEX
+                    cv.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                else:
+                    cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                    # Draw a label with a name below the face
+                    cv.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv.FILLED)
+                    font = cv.FONT_HERSHEY_DUPLEX
+                    cv.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
+        # Display the resulting image
+        cv.imshow("Video", frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv.destroyAllWindows()
+    
+face_recognition()
--- src/tests/fr_test.py
+++ src/tests/fr_test.py
@@ -1,101 +1,53 @@
-from posixpath import expanduser
 import face_recognition as fr
 import sys
-import cv2 as cv 
 import numpy as np
+import cv2 as cv
 
 from os.path import expanduser
-from PIL import ImageDraw as imgd, Image as img
 
-# this checks all the available web cams in computer and exits
-# if there is none, and returns True or 1 if there is any
-def av_cams():
-    index, arr = 0, []
+# this checks all the available web cams in computer and exits if there
+# is none, and returns True or 1 if there is any
 
-    while True:
-        cap = cv.VideoCapture(index)
+if __name__ == "__main__":
+    __PATH__ = f"{expanduser('~')}/temporary/capstone/sample/"
+    
+    # load face
+    ref_face = fr.load_image_file(f"{__PATH__}/test_1.png")    
+    rf_encoding = fr.face_encodings(ref_face)[0]
 
-        if not cap.read()[0]:
+    # video capture
+    vid = cv.VideoCapture(0)
+
+    # loop
+    while True:   
+        ret, frame = vid.read()
+        small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        cv.imshow('frame', frame)
+
+        face_locations = fr.face_locations(rgb_small_frame)
+        face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
+
+        for fe in face_encodings:
+            matches = fr.compare_faces(rf_encoding, fe)
+
+        for (top, right, bottom, left) in face_locations:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+
+        # Display the resulting image
+        cv.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
-        else:
-            arr.append(index)
-        cap.release()
-        index += 1
-
-    if arr == []:
-        pass
-        # raise SystemExit(f"{C.RED+C.BOLD}> No camera available.{C.END}")
-    else:
-        input(f"\33[1;32m> All available cameras: {[num+' '+cam for num, cam in enumerate(arr)]}\33[0m\nPress any key to clear ...")
-        sys.stdout.write("\033[K")
-        return True
-
-av_cams()
-
-_PATH_ = f"{expanduser('~')}/devel/capstone/test_faces/"
-
-biden_image = fr.load_image_file(f"{_PATH_}biden1.jpg")
-bid_encode = fr.face_encodings(biden_image)[0]
-
-biden_image2 = fr.load_image_file(f"{_PATH_}biden2.jpg")
-bid2_fl = fr.face_locations(biden_image2)
-bid2_encode = fr.face_encodings(biden_image2, bid2_fl)
-
-obama_image = fr.load_image_file(f"{_PATH_}obama1.jpg")
-obama_encode = fr.face_encodings(obama_image)[0]
-
-obama_image2 = fr.load_image_file(f"{_PATH_}obama2.jpg")
-obama2_encode = fr.face_encodings(obama_image2)[0]
 
 
-# unknown_image = fr.load_image_file(f"{_PATH_}f2.jpg")
-# u_encode = fr.face_encodings(unknown_image)[0]
-
-# feed = cv.VideoCapture(0)
-
-known_face_encodings = [
-    obama_encode,
-    bid_encode
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
-]
-
-pil_image = img.fromarray(biden_image2)
-draw = imgd.Draw(pil_image)
-
-for (top, right, bottom, left), face_encoding in zip(bid2_fl, bid2_encode):
-    # See if the face is a match for the known face(s)
-    matches = fr.compare_faces(known_face_encodings, face_encoding)
-
-    name = "Unknown"
-
-    face_distances = fr.face_distance(known_face_encodings, face_encoding)
-    best_match_index = np.argmin(face_distances)
-    if matches[best_match_index]:
-        name = known_face_names[best_match_index]
-
-    # Draw a box around the face using the Pillow module
-    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-
-    # Draw a label with a name below the face
-    text_width, text_height = draw.textsize(name)
-    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-    draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-
-
-# Remove the drawing library from memory as per the Pillow docs
-del draw
-
-# Display the resulting image
-pil_image.show()
-
-
-
-
-"""results = fr.compare_faces([obama_encode], bid2_encode)
-
-print("Is the unknown face a picture of Obama? {}".format(results))
-# print("Is the unknown face a picture of Biden? {}".format(results[1]))
-# print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))"""
+vid.release()
+cv.destroyAllWindows()
