49def8188f9a8b5e4cf58bc79bb4d6892cbb3c3a
iaacornus <iaacornus.devel@gmail.com>
2022-05-14 01:25:43 +0800

    simplified the code

--- src/system/utils/update.py
+++ src/system/utils/update.py
@@ -8,24 +8,21 @@ from misc.colors import colors as C
 
 
 def update():
-    ret = access()
     HOME = os.path.expanduser("~")
     
-    if ret:
+    if access():
         if not os.path.exists(f"{HOME}/repo"):        
             os.system(
-                "git clone https://github.com/testno0/repo $HOME/ &> /dev/null"
+                f"git clone https://github.com/testno0/repo {HOME} &> /dev/null"
             )
         else:
             print(f"{C.BOLD+C.GREEN} User system setup passed.{C.END}")
             os.system(f"cd {HOME}/repo/ && git pull")
     else:
-        # false phase, pass : #? passing
-        os.system("rm -rf {HOME}/repo/")
-        
+        os.system(f"rm -rf {HOME}/repo/")        
         print(
             f"{C.BOLD+C.RED}> Verification error",
-            "repository was nuked by system for security.{C.END}"
+            f"repository was nuked by system for security.{C.END}"
         )            
         #systemctl poweroff
 
