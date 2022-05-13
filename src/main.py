f246d39961abb07d21938b98197e4a538f19d2da
iaacornus <iaacornus.devel@gmail.com>
2022-05-13 18:10:37 +0800

    complied with pep8 more, and removed unused variables

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from sys import argv as INPT, stdout
+import sys
 from difflib import SequenceMatcher as SM
 from os import path
 from time import sleep
@@ -13,24 +13,24 @@ def main():
     """initiate the system, use try, except, else block to catch errors
     and to organize the procedures based on the cases the system gives."""
 
-    HOME = path.expanduser('~')
+    HOME_ = path.expanduser("~")
 
-    with open(f"{HOME}/.att_sys/user_info") as info:
+    with open(f"{HOME_}/.att_sys/user_info") as info:
         source = info.readlines()
 
-    receiver_email, user = source[0].rstrip().strip(), source[1].rstrip().strip() 
-    password, school_name = source[2].rstrip().strip(), source[3].rstrip().strip()
-    sysINIT = System(HOME, "https://github.com/testno0/repo", receiver_email)
+    receiver_email = source[0].rstrip().strip() 
+    school_name = source[3].rstrip().strip()
+    sys_initiate = System(HOME_, "https://github.com/testno0/repo", receiver_email)
 
     try:
         print(f"{C.GREEN+C.BOLD}> Fetching data.{C.END}")
-        if not path.exists(f"{HOME}/repo"):
-            print(f"""\
-                {C.GREEN+C.BOLD}> The repository is not setup. Setting up the repository.{C.END}
-            """)
-            studentDATA, teacherDATA = sysINIT.setup(school_name)        
+        if not path.exists(f"{HOME_}/repo"):
+            print(
+                f"{C.GREEN+C.BOLD}> The repository is not setup. Setting up the repository.{C.END}"
+            )
+            student_data, teacher_data = sys_initiate.setup(school_name)        
         else:
-            studentDATA, teacherDATA = sysINIT.get_data()
+            student_data, teacher_data = sys_initiate.get_data()
     except ConnectionError: # add other exceptions later
         raise SystemExit(f"{C.RED+C.BOLD}> Connection Error.{C.END}")
     except KeyboardInterrupt:
@@ -40,30 +40,28 @@ def main():
     else:
         # notify the user
         print(f"{C.GREEN+C.BOLD+C.BLINK}> System ready.{C.END}", end="\r")
-        
-        # init free time
-        sleep(5)
-        # remove the messages
-        stdout.write("\033[K")
+
+        sleep(5) # init free time
+        sys.stdout.write("\033[K") # remove the messages
        
         email = Email()
         while True:
-            pass
-            cardID = INPT[0]            
+            cardID = sys.argv[0]            
                         
-            for ID in studentDATA:
+            for ID in student_data:
                 if SM(None, cardID, ID).ratio() == 1:
                     print(f"{C.GREEN+C.BOLD}> Student recognized.{C.END}")
                     email.send(
                         "student true",
                         school_name,
-                        studentDATA[ID][0]
+                        student_data[ID][0]
                     )
                 else:
                     print(f"{C.RED+C.BOLD}> Error.{C.END}")
                     # leave at blank first
-                    email.send("student true",
-                               school_name,
-                               studentDATA
-                    )[ID][0]
+                    email.send(
+                        "student true",
+                        school_name,
+                        student_data[ID][0]
+                    )
             continue
