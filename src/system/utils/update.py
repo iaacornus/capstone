a0544c06aeab54263f1d361a89949e261a435cf9
iaacornus <iaacornus.devel@gmail.com>
2022-05-14 01:39:13 +0800

    synched with changes from access.py

--- src/system/utils/update.py
+++ src/system/utils/update.py
@@ -10,7 +10,7 @@ from misc.colors import colors as C
 def update():
     HOME = os.path.expanduser("~")
     
-    if access():
+    if access(HOME):
         if not os.path.exists(f"{HOME}/repo"):        
             os.system(
                 f"git clone https://github.com/testno0/repo {HOME} &> /dev/null"
