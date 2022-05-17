bf2e1fe097ef664e38fed0ed141a9f6f59bc9afa
iaacornus <iaacornus.devel@gmail.com>
2022-05-17 12:12:21 +0800

    complied with c++ recommendations

--- rfid_locker/src/main.cpp
+++ rfid_locker/src/main.cpp
@@ -9,31 +9,42 @@
 #define RST_PIN 9
 Chrono mychrono;
 
-String UID = "43 12 9F 94", UID_2 = "B3 E5 F 95";
-bool lock = true, check, check_2;
-
 Servo servo;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-void lcd_print(String message, int x, int y, int sdelay, bool clear=false, bool s_clear=false) {
-  if (clear) {
+String UID = "43 12 9F 94", UID_2 = "B3 E5 F 95";
+bool lock = true;
+
+void lcd_print(
+    String message,
+    int x, int y,
+    int sdelay,
+    bool clear=false,
+    bool s_clear=false
+  )
+{
+  if (clear)
+  {
     lcd.clear();
   }
 
   lcd.setCursor(x, y);
   lcd.print(message);
 
-  if (sdelay != 0) {
+  if (sdelay != 0)
+  {
     delay(sdelay);
   }
 
-  if (s_clear) {
+  if (s_clear)
+  {
     lcd.clear();
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   servo.write(70);
   lcd.init();
@@ -43,16 +54,19 @@ void setup() {
   rfid.PCD_Init();
 }
 
-void loop() {
+void loop()
+{
   lcd_print("Welcome!", 6, 1, 0);
   lcd_print("Please scan the card", 0, 2, 0);
 
-  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+  {
     return;
   }
 
   String ID = "";
-  for (byte i = 0; i < rfid.uid.size; i++) {
+  for (byte i = 0; i < rfid.uid.size; i++)
+  {
     ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
     ID.concat(String(rfid.uid.uidByte[i], HEX));
   }
@@ -61,22 +75,25 @@ void loop() {
 
   lcd_print("Scanning", 0, 0, true);
   lcd_print("Please wait", 0, 1, false);
+
   // start rfid card recognition
-  if (lock) {
-    if (ID.substring(1) == UID || ID.substring(1) == UID_2) {
-      lcd_print("Door opened.", 4, 1, true);      
+  if (lock)
+  {
+    if (ID.substring(1) == UID || ID.substring(1) == UID_2)
+    {
+      lcd_print("Door opened.", 4, 1, true);
       servo.write(160);
 
-      if (rfid.PICC_ReadCardSerial()) {
+      if (rfid.PICC_ReadCardSerial() || mychrono.hasPassed(3000))
+      {
         lcd_print("lock here 2.", 4, 1, 0, true, true);
         delay(2000);
-      } else {
-        if (mychrono.hasPassed(3000)) {
-          lcd_print("lock here.", 4, 1, 0, true, true);
-          delay(2000);
-        }
+
       }
-    } else {
+      lock = true;
+    }
+    else
+    {
       lcd_print("Access denied.", 4, 1, 0, true, true);
       delay(3000);
     }
