51e9021a5f5cc3e6fd8aacdd7527a6140db899da
iaacornus <iaacornus.devel@gmail.com>
2022-05-17 20:12:05 +0800

    fixed the process stdout

--- src/system/utils/update.py
+++ src/system/utils/update.py
@@ -13,27 +13,24 @@ def update(setup):
     HOME = os.path.expanduser("~")
     console = Console()
 
-    with console.status("[bold]> Updating repository ...[/bold]."):
-        if access(HOME):
+
+    if access(HOME):
+        with console.status("[bold]> Updating repository ...[/bold]."):
             if not os.path.exists(f"{HOME}/repo"):
+                console.log("[bold red][-] Local repository not found[/bold red]")
                 console.log(
-                    "[bold red]>>> Local repository not found.[/bold red]\n"
-                    + "[bold]>>> Cloning the repository ...[/bold]"
+                    "[bold bright_cyan][+] Cloning the repository.[/bold bright_cyan]"
                 )
                 os.system(
                     f"git clone https://github.com/testno0/repo {HOME} &> /dev/null"
                 )
             else:
-                console.log("[bold]>>> Pulling updates ...[/bold]")
+                console.log("[bold bright_cyan][+] Pulling updates ...[/bold bright_cyan]")
                 os.system(f"cd {HOME}/repo/ && git pull")
-        else:
-            os.system(f"rm -rf {HOME}/repo/")
-            print(
-                f"{C.BOLD+C.RED}> Verification error",
-                f"repository was nuked by system for security.{C.END}",
-                end="\n"
-            )
-            #systemctl poweroff
+    else:
+        os.system(f"rm -rf {HOME}/repo/")
+        console.log("[bold red][-] Verification error.\n> Nuking the repository ...[/bold red]")
+        os.system("systemctl poweroff")
 
 if __name__ == "__main__":
     update()
