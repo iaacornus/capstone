0170db34aa76b7f720f5db03c6082f41e1fcc9c5
iaacornus <iaacornus.devel@gmail.com>
2022-05-17 16:26:03 +0800

    class bug fix

--- src/demo.py
+++ src/demo.py
@@ -5,15 +5,15 @@ from rich.console import Console
 
 from face_recog import face_recognition
 from function import av_cams
-from misc.colors import Color as C
+
 
 def demo():
     console = Console()
     av_cams_ = av_cams()
     path_ = f"{expanduser('~')}/temporary/capstone/sample/"
 
-    with console.status("[bold]> Loading face images ...[/bold]") as console:
-        console.log("[bold]> Loading face images ...[/bold]")
+    with console.status("[bold]> Loading user data ...[/bold]"):
+        console.log("[bold]>>> Loading face images ...[/bold]")
         # load face references from path_.
         ref_face = fr.load_image_file(f"{path_}/test_1.png") # ezekiel lopez encoding
         ref_face_2 = fr.load_image_file(f"{path_}/test_2.png") # laisie angela donato encoding
@@ -21,7 +21,7 @@ def demo():
         ref_face_4 = fr.load_image_file(f"{path_}/test_4.png") # raven gose encoding
         ref_face_5 = fr.load_image_file(f"{path_}/test_5.png") # fiona leigh pagtama encoding
 
-        console.log("[bold green]> Face loaded successfully.[/bold green]")
+        console.log("[bold green]>>> Face loaded successfully.[/bold green]")
 
         #----------------------------------------------------------------
         # encode the faces.
@@ -32,11 +32,11 @@ def demo():
         rf_encoding4 = fr.face_encodings(ref_face_4)[0]
         rf_encoding5 = fr.face_encodings(ref_face_5)[0]
 
-        console.log("[bold green]> Faces encoded successfully.[/bold green]")
+        console.log("[bold green]>>> Faces encoded successfully.[/bold green]")
 
     # load the function with the parameters it needs, unpack the tuple
     # for the decoding.
-    with console.status("[bold]> Preparing function ...[/bold]") as console:
+    with console.status("[bold]> Preparing function ...[/bold]"):
         face_recognition(
             av_cams_, console,
             face_encodings_= (
