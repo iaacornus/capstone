baf26c9123eb657dcb6ed80a862455f3e730b4d9
iaacornus <iaacornus.devel@gmail.com>
2022-07-29 22:25:48 +0800

    fix email message formatting

--- src/bin/code_email.py
+++ src/bin/code_email.py
@@ -36,68 +36,130 @@ class Email:
             )
 
         if access == "setup":
+            with open("../msg/setup.msg", "r", encoding="utf-8") as msg_file:
+                msg = msg_file.read()
+
             msg["Subject"] = "Secure access phrase"
             msg.set_content(
-                f"""\
-                <!DOCTYPE html>
-                <html>
-                    </body>
-                        <p align=justify>
-                            Hello, TapTap is here to deliver the secure passphrase requested by: <b>{self.receiver_email} ({self.user}) from {school_name}.</b>,<br><br>
-                            <em>DO NOT SHARE THIS CODE FOR USE IN SETUP/GIT PULL OF THE DATABASE</em>. This is the secure phrase for your access of the student and teacher data in repository setup: <br><br><center><b><code>{phrase}</code></b></center>
-                        </p>
+                (
+                    msg
+                        .replace(
+                            "{RECEIVER_EMAIL}",
+                            self.receiver_email
+                        )
+                        .replace(
+                            "{USER}",
+                            self.user
+                        )
+                        .replace(
+                            "{SCHOOL_NAME}",
+                            school_name
+                        )
+                        .replace(
+                            "{HOSTNAME}",
+                            socket.gethostname()
+                        )
+                        .replace(
+                            "{USERNAME}",
+                            os.getlogin()
+                        )
+                        .replace(
+                            "{IP_ADDR}",
+                            socket.gethostbyname(socket.gethostname())
+                        )
+                        .replace(
+                            "{LOC}",
+                            geocoder.ip("me")
+                        )
+                        .replace(
+                            "{DATETIME}",
+                            datetime.now().strftime(
+                                "%d/%m/%Y %H:%M:%S"
+                            )
+                        )
 
-                        <p align=justify>
-                            If you are not trying to access, ignore this email. The details of the computer are:<br><br>User: <i>{os.getlogin()}</i><br>HOSTNAME: <i>{socket.gethostname()}</i><br>IP address: <i>{socket.gethostbyname(socket.gethostname())}</i><br>Tracked from: <i>{geocoder.ip('me')}) at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}</i>.<blockquote>Cheers,<br>TapTap team</blockquote>
-                        </p>
-                    </body>
-                </html>
-                """,
+                ),
                 subtype="html"
             )
 
         elif access == "alert":
+            with open("../msg/setup.msg", "r", encoding="utf-8") as msg_file:
+                msg = msg_file.read()
+
             msg["Subject"] = "Breach attempt alert."
             msg.set_content(
+                (
+                    msg
+                        .replace(
+                            "{RECEIVER_EMAIL}",
+                            self.receiver_email
+                        )
+                        .replace(
+                            "{USER}",
+                            self.user
+                        )
+                        .replace(
+                            "{SCHOOL_NAME}",
+                            school_name
+                        )
+                        .replace(
+                            "{USERNAME}",
+                            os.getlogin()
+                        )
+                        .replace(
+                            "{HOSTNAME}",
+                            socket.gethostname()
+                        )
+                        .replace(
+                            "{IP_ADDR}",
+                            socket.gethostbyname(socket.gethostname())
+                        )
+                        .replace(
+                            "{LOC}",
+                            geocoder.ip("me")
+                        )
+                        .replace(
+                            "{DATETIME}",
+                            datetime.now().strftime(
+                                "%d/%m/%Y %H:%M:%S"
+                            )
+                        )
+                ),
+                subtype="html"
+            )
+            msg.set_content(
                 f"""\
-                <!DOCTYPE html>
-                <html>
-                    </body>
-                        <p align=justify>
-                            Hello, TapTap is here to alert <b>{self.receiver_email} ({self.user}) from {school_name} of possible breach alert.</b><br><br>
-                            Your device has been receiving various passphrase mismatch. Due to the setup of device, it is shutting down as you receive this email. You can access it later on.
-                        </p>
 
-                        <p align=justify>
-                            If you are not trying to access, ignore this email. The details of the computer are:<br><br>User: <i>{os.getlogin()}</i><br>HOSTNAME: <i>{socket.gethostname()}</i><br>IP address: <i>{socket.gethostbyname(socket.gethostname())}</i><br>Tracked from:<i> {geocoder.ip('me')}) at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}</i>.
-                        </p>
-
-                        <p align=justify>
-                            This was done as security measure, the email in situation that met the criterion is inevitable.<br><blockquote>Cheers,<br>TapTap team</blockquote>
-                        </p>
-                    </body>
-                </html>
                 """,
                 subtype="html"
             )
 
         elif access == "student true":
+            with open("../msg/setup.msg", "r", encoding="utf-8") as msg_file:
+                msg = msg_file.read()
+
             msg["Subject"] = f"{student_name} registered"
             msg.set_content(
-                f"""\
-                <!DOCTYPE html>
-                <html>
-                    </body>
-                        <p align=justify>
-                            Hello, TapTap is here to notify that {student_name} of {school_name} has finally arrived his class at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}, and is present in school today. Thank you!
-                        </p>
+                (
+                    msg
+                        .replace(
+                            "{STUDENT_NAME}",
+                            student_name
+                        )
+                        .replace(
+                            "{SCHOOL_NAME}",
+                            school_name
+                        )
+                        .replace(
+                            "{DATETIME}",
+                            datetime.now().strftime(
+                                "%d/%m/%Y %H:%M:%S"
+                            )
+                        )
 
-                    </body>
-                </html>
-                """,
+                ),
                 subtype="html"
             )
-
         try:
             with console.status(
                     "[bold magenta][+] Sending email ...[/bold magenta]",
