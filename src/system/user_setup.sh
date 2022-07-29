e2bd67eeebb5eaa409d6b6b9e6c45abb01e2d688
iaacornus <iaacornus.devel@gmail.com>
2022-07-29 22:36:29 +0800

    update variable name

--- src/system/user_setup.sh
+++ src/system/user_setup.sh
@@ -9,20 +9,20 @@ if [ ! -d "$HOME/.att_sys/user_info" ]; then
     read -p "└─╼ Enter the email for access codes and reports: " email
     read -p "└─╼ Enter the username: " user_name
     read -p "└─╼ Enter the school name: " school_name
-    
+
     # generate user password
-    password=$(python $HOME/.att_sys/system/utils/password_gen.py)
+    password_hash=$(python $HOME/.att_sys/system/utils/password_gen.py)
     echo -e "\e[1;32m> Your password is:\e[1;0m $password"
 
     # append user information and password to a file
     echo -e "\e[1;32m> Appending user info ...\e[0m"
-    echo -e "$email\n$user_name\n$password\n$school_name" > $HOME/.att_sys/user_info
+    echo -e "$email\n$user_name\n$password_hash\n$school_name" > $HOME/.att_sys/user_info
 
     python utils/update.py
 
     sec=10
     while [ $sec -ge 0 ]; do
-        echo -e "\e[32m> Setup done! Rebooting after :" $sec"s ...\e[0m\r" 
+        echo -e "\e[32m> Setup done! Rebooting after :" $sec"s ...\e[0m\r"
         let "sec=sec-1"
         sleep 1
     done
