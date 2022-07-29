f329cfd062156915f243231febe82084c6e14e87
iaacornus <iaacornus.devel@gmail.com>
2022-07-29 22:33:20 +0800

    hash the password for saving and remove the printout after 10s

--- src/system/utils/password_gen.py
+++ src/system/utils/password_gen.py
@@ -1,12 +1,26 @@
-import random
-import string
+from random import choice
+from string import (
+    ascii_lowercase,
+    ascii_uppercase,
+    punctuation,
+    digits
+)
+from hashlib import sha256
+from time import sleep
 
 
 str_set = [
-        string.ascii_lowercase,
-        string.ascii_uppercase,
-        string.punctuation,
-        string.digits
+        ascii_lowercase,
+        ascii_uppercase,
+        punctuation,
+        digits
     ]
+password = "".join([choice(choice(str_set)) for x in range(32)])
 
-print("".join([random.choice(random.choice(str_set)) for x in range(32)]))
+for n in range(10):
+    print(f"Your password is: {password}\nRemoving in {10-n}", end="\r")
+    sleep(1)
+
+print(
+    sha256(password.encode("utf-8")).hexdigest()
+)
