0d104667428b3221870cfc36dbe6c3923a5529e8
iaacornus <iaacornus.devel@gmail.com>
2022-07-29 23:11:59 +0800

    turn password_gen into function

--- src/system/utils/password_gen.py
+++ src/system/utils/password_gen.py
@@ -9,18 +9,23 @@ from hashlib import sha256
 from time import sleep
 
 
-str_set = [
-        ascii_lowercase,
-        ascii_uppercase,
-        punctuation,
-        digits
-    ]
-password = "".join([choice(choice(str_set)) for x in range(32)])
+def password_gen() -> None:
+    str_set = [
+            ascii_lowercase,
+            ascii_uppercase,
+            punctuation,
+            digits
+        ]
+    password = "".join([choice(choice(str_set)) for x in range(32)])
 
-for n in range(10):
-    print(f"Your password is: {password}\nRemoving in {10-n}", end="\r")
-    sleep(1)
+    for n in range(10):
+        print(f"Your password is: {password}\nRemoving in {10-n}", end="\r")
+        sleep(1)
 
-print(
-    sha256(password.encode("utf-8")).hexdigest()
-)
+    print(
+        sha256(password.encode("utf-8")).hexdigest()
+    )
+
+
+if __name__ == "__main__":
+    password_gen()
