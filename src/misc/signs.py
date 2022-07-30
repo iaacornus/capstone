f770bb275e5cb39d3f5813df624963f0adc63ed9
iaacornus <iaacornus.devel@gmail.com>
2022-07-30 14:16:52 +0800

    include ansi color codes

--- src/misc/signs.py
+++ src/misc/signs.py
@@ -1,6 +1,12 @@
 class Signs:
-    PROC: str = "[bold][ PROC ][/bold]"
-    FAIL: str = "[bold][ [red]FAIL[/red] ][/bold]"
-    PASS: str = "[bold][ [green]PASS[/green] ][/bold]"
-    INFO: str = "[bold][ INFO ][/bold]"
-    INPT: str = "[bold][ INPT ][/bold]"
+    RPROC: str = "[bold][ PROC ][/bold]"
+    RFAIL: str = "[bold][ [red]FAIL[/red] ][/bold]"
+    RPASS: str = "[bold][ [green]PASS[/green] ][/bold]"
+    RINFO: str = "[bold][ INFO ][/bold]"
+    RINPT: str = "[bold][ INPT ][/bold]"
+
+    PROC: str = "\033[1m[ PROC ]\033[0m"
+    FAIL: str = "\033[1m[ \033[31mFAIL\033[0m\033[1m ]\033[0m"
+    PASS: str = "\033[1m[ \033[32mPASS\033[0m\033[1m ]\033[0m"
+    INFO: str = "\033[1m[ INFO ]\033[0m"
+    INPT: str = "\033[1m[ INPT ]\033[0m"
