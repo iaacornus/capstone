68ac82fe89f054ee1f3fd461eab17476be5bb92d
iaacornus <iaacornus.devel@gmail.com>
2022-07-30 12:17:04 +0800

    stdout signs

---
+++ src/misc/msg/signs.py
@@ -0,0 +1,6 @@
+class Signs:
+    PROC: str = "[bold][ PROC ][/bold]"
+    FAIL: str = "[bold][ [red]FAIL[/red] ][/bold]"
+    PASS: str = "[bold][ [green]PASS[/green] ][/bold]"
+    INFO: str = "[bold][ INFO ][/bold]"
+    INFO: str = "[bold][ INPT ][/bold]"
