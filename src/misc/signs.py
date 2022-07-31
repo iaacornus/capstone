719947cc0e29c53466175392ff73527996bfc032
iaacornus <iaacornus.devel@gmail.com>
2022-07-31 22:58:16 +0800

    remove rich completely

--- src/misc/signs.py
+++ src/misc/signs.py
@@ -1,10 +1,4 @@
 class Signs:
-    RPROC: str = "[bold][ PROC ][/bold]"
-    RFAIL: str = "[bold][ [red]FAIL[/red] ][/bold]"
-    RPASS: str = "[bold][ [green]PASS[/green] ][/bold]"
-    RINFO: str = "[bold][ INFO ][/bold]"
-    RINPT: str = "[bold][ INPT ][/bold]"
-
     PROC: str = "\033[1m[ PROC ]\033[0m"
     FAIL: str = "\033[1m[ \033[31mFAIL\033[0m\033[1m ]\033[0m"
     PASS: str = "\033[1m[ \033[32mPASS\033[0m\033[1m ]\033[0m"
